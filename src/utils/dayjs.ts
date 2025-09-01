b1aba51c019c0b825899ff1b9aa848a8c3a24ff9
ellen.kuo <ellen.kuo@patrioimemory.com>
2025-09-01 13:43:35 +0800

    feat: add toUnix function

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -8,12 +8,16 @@ export const formatDate = (date: string | Date) => {
   return dayjs(date).format("YYYY/MM/DD");
 };
 
+export const toUnix = (date: string | Date) => {
+  return dayjs(date).unix();
+};
+
 export const getStartOfMonth = () => {
-  return dayjs().startOf("month").format("YYYY-MM-DD");
+  return dayjs().startOf("month");
 };
 
 export const getEndOfMonth = () => {
-  return dayjs().endOf("month").format("YYYY-MM-DD");
+  return dayjs().endOf("month");
 };
 
 export const getDaysInMonth = () => {
