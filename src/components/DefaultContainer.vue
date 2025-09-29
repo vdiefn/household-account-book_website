9dee96ba41cc681338bd8fe42baecaf39e756dd8
Ellen <ddhkxxhxx@hotmail.com>
2025-09-29 13:00:51 +0800

    feat(defaultContainer): modify css layout

--- src/components/DefaultContainer.vue
+++ src/components/DefaultContainer.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup></script>
 <template>
-  <section class="default-container">
+  <div class="default-container">
     <header class="default-header">
       <slot name="header"></slot>
     </header>
@@ -8,30 +8,31 @@
     <footer class="default-footer">
       <slot name="footer"></slot>
     </footer>
-  </section>
+  </div>
 </template>
 <style lang="scss" scoped>
 .default-container {
   border: 1px solid $background-border;
-  height: 100%;
   width: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   border-radius: $background-radius;
   padding: 1rem;
   background-color: $background-color;
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
-}
+  overflow-y: auto;
 
-.default-header {
-  display: flex;
-  align-items: center;
-  color: $default-text-color;
-}
+  .default-header {
+    display: flex;
+    align-items: center;
+    color: $default-text-color;
+  }
 
-.default-footer {
-  display: flex;
-  justify-content: center;
-  margin: 2rem auto;
+  .default-footer {
+    display: flex;
+    justify-content: center;
+    margin: 1rem auto;
+  }
 }
 </style>
